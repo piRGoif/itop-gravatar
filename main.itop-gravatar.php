840cea62b0a991e1fc0e5ac447098343dd6c5f47
Pierre Goiffon <pierre.goiffon@combodo.com>
2018-12-03 09:43:28 +0100

    Add custom attr in form

--- main.itop-gravatar.php
+++ main.itop-gravatar.php
@@ -3,6 +3,79 @@
 @include_once('../approot.inc.php');
 require_once(APPROOT.'application/utils.inc.php');
 
+
+class PersonCustomField implements iApplicationUIExtension
+{
+	/**
+	 * @inheritdoc
+	 */
+	public function OnDisplayProperties($oObject, WebPage $oPage, $bEditMode = false)
+	{
+		$oPage->add('PersonCUSTOM-prop');
+		//FIXME should add JS here !
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function OnDisplayRelations($oObject, WebPage $oPage, $bEditMode = false)
+	{
+		// nothing
+		IssueLog::Info('Gravatar dispRel');
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function OnFormSubmit($oObject, $sFormPrefix = '')
+	{
+		// nothing
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function OnFormCancel($sTempId)
+	{
+		// nothing
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function EnumUsedAttributes($oObject)
+	{
+		// nothing
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function GetIcon($oObject)
+	{
+		// nothing
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function GetHilightClass($oObject)
+	{
+		// nothing
+		return HILIGHT_CLASS_NONE;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function EnumAllowedActions(DBObjectSet $oSet)
+	{
+		// nothing
+		return array();
+	}
+}
+
+
 class CalendarViewUIextension implements iPageUIExtension
 {
 
